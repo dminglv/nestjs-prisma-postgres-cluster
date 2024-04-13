25becb57399ffffc9e03f04db1448d0a1fbd107a
dmitrii-syazan <da.mingalev@gmail.com>
2024-04-13 22:40:34 +0800

    upd

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,29 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { readReplicas } from '@prisma/extension-read-replicas';
+import { ConfigService } from '@nestjs/config';
 
-import { PrismaClientExtended } from './custom-prisma-client';
+const customPrismaClient = (prismaClient: PrismaClient, url?: string) => {
+  if (url) {
+    return prismaClient.$extends(
+      readReplicas({
+        url,
+      }),
+    );
+  }
+  return prismaClient.$extends({});
+};
 
 @Injectable()
 export class PrismaService
-  extends PrismaClientExtended
+  extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
+
+  customPrismaClient: CustomPrismaClient;
   async onModuleInit() {
     await this.$connect();
   }
@@ -14,4 +31,18 @@ export class PrismaService
   async onModuleDestroy() {
     await this.$disconnect();
   }
+
+  get client() {
+    if (!this.customPrismaClient) {
+      const url =
+        this.configService.get<string>('ENVIRONMENT') === 'local-dev'
+          ? undefined
+          : this.configService.get<string>('DATABASE_URL_REPLICA');
+      this.customPrismaClient = customPrismaClient(this, url);
+    }
+
+    return this.customPrismaClient;
+  }
 }
+
+export type CustomPrismaClient = ReturnType<typeof customPrismaClient>;
